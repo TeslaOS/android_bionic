a490b14965a410e9cd9c4ce8af1bcb5858575782
NoelMacwan <yulespencer@gmail.com>
2015-09-09 09:01:44 +0530

    Revert 'optimized cortex-a7 and cortex-a53 (memset/memcpy)'
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>

--- libc/arch-arm/cortex-a53/cortex-a53.mk
+++ libc/arch-arm/cortex-a53/cortex-a53.mk
@@ -1,4 +1,5 @@
 libc_bionic_src_files_arm += \
+    arch-arm/bionic/memchr.v7a.S \
     arch-arm/cortex-a53/bionic/memcpy.S \
     arch-arm/cortex-a53/bionic/__strcat_chk.S \
     arch-arm/cortex-a53/bionic/__strcpy_chk.S \
@@ -13,3 +14,4 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/strlen.S \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/krait/bionic/memmove.S
+
